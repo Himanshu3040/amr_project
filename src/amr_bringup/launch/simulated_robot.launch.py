bb82a888d4d09cb0313f338c597987fe973b8625
Himanshu3040 <himanshurokade543@gmail.com>
2026-05-08 11:30:46 +0530

    refresh

---
+++ src/amr_bringup/launch/simulated_robot.launch.py
@@ -0,0 +1,41 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+
+    use_slam_arg = DeclareLaunchArgument(
+        "use_slam",
+        default_value="false"
+    )
+
+    gazebo = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("amr_description"),
+            "launch",
+            "gazebo.launch.py"
+        ),
+    )
+    
+    controller = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("amr_controller"),
+            "launch",
+            "controller.launch.py"
+        ),
+        launch_arguments={
+            "use_simple_controller": "False",
+            "use_python": "False"
+        }.items(),
+    )
+    
+    return LaunchDescription([
+        use_slam_arg,
+        gazebo,
+        controller
+    ])
