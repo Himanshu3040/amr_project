f366cd204a5413c8cfbf12cb59196801701cd49c
Himanshu3040 <himanshurokade543@gmail.com>
2026-05-09 17:02:05 +0530

    Done Sensor Fusion

--- src/amr_controller/launch/controller.launch.py
+++ src/amr_controller/launch/controller.launch.py
@@ -4,6 +4,41 @@ from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 from launch.conditions import UnlessCondition, IfCondition
 
+def noisy_controller(context, *args, **kwargs):
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    use_python = LaunchConfiguration("use_python")
+    wheel_radius = float(LaunchConfiguration("wheel_radius").perform(context))
+    wheel_separation = float(LaunchConfiguration("wheel_separation").perform(context))
+    wheel_radius_error = float(LaunchConfiguration("wheel_radius_error").perform(context))
+    wheel_separation_error = float(LaunchConfiguration("wheel_separation_error").perform(context))
+
+    noisy_controller_py = Node(
+        package="amr_controller",
+        executable="noisy_controller.py",
+        parameters=[
+            {"wheel_radius": wheel_radius + wheel_radius_error,
+             "wheel_separation": wheel_separation + wheel_separation_error,
+             "use_sim_time": use_sim_time}],
+        condition=IfCondition(use_python),
+    )
+
+    noisy_controller_cpp = Node(
+        package="amr_controller",
+        executable="noisy_controller",
+        parameters=[
+            {"wheel_radius": wheel_radius + wheel_radius_error,
+             "wheel_separation": wheel_separation + wheel_separation_error,
+             "use_sim_time": use_sim_time}],
+        condition=UnlessCondition(use_python),
+    )
+
+    return [
+        noisy_controller_py,
+        noisy_controller_cpp,
+    ]
+
+
+
 def generate_launch_description():
     
     use_sim_time_arg = DeclareLaunchArgument(
@@ -25,6 +60,14 @@ def generate_launch_description():
     wheel_separation_arg = DeclareLaunchArgument(
         "wheel_separation",
         default_value="0.460",
+    )
+    wheel_radius_error_arg = DeclareLaunchArgument(
+        "wheel_radius_error",
+        default_value="0.005",
+    )
+    wheel_separation_error_arg = DeclareLaunchArgument(
+        "wheel_separation_error",
+        default_value="0.02",
     )
     
     use_sim_time = LaunchConfiguration("use_sim_time")
@@ -84,6 +127,9 @@ def generate_launch_description():
             ),
         ]
     )
+    
+    noisy_controller_launch = OpaqueFunction(function=noisy_controller)
+
     return LaunchDescription(
         [
             use_sim_time_arg,
@@ -91,8 +137,11 @@ def generate_launch_description():
             use_python_arg,
             wheel_radius_arg,
             wheel_separation_arg,
+            wheel_radius_error_arg,
+            wheel_separation_error_arg,
             joint_state_broadcaster_spawner,
             wheel_controller_spawner,
-            simple_controller
+            simple_controller,
+            noisy_controller_launch
         ]
     )
