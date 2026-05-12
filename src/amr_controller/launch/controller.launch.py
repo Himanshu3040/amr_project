54a0c7bf1030a2d56224a10c2bcfb88e8fb41f9e
Himanshu3040 <himanshurokade543@gmail.com>
2026-05-12 11:40:34 +0530

    Done Lidar Safety Stop

--- src/amr_controller/launch/controller.launch.py
+++ src/amr_controller/launch/controller.launch.py
@@ -60,7 +60,7 @@ def generate_launch_description():
     )
     wheel_separation_arg = DeclareLaunchArgument(
         "wheel_separation",
-        default_value="0.460",
+        default_value="0.480",
     )
     wheel_radius_error_arg = DeclareLaunchArgument(
         "wheel_radius_error",
